04bac95e2ce0de3674998afe7d644a8a278d95b3
MBeshkov <u05mb19@abdn.ac.uk>
2024-04-05 22:11:51 +0100

    home page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ import './App.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <Link to="/feedback-summary" className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-purple-500 rounded shadow ripple hover:shadow-lg hover:bg-purple-600 focus:outline-none">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-200 via-blue-200 to-purple-200">
+            <h1 className="mb-6 text-3xl font-bold text-gray-900">Off-chain Project Capabilities</h1>
+            <Link to="/feedback-summary" className="w-full max-w-xs px-4 py-2 mb-4 text-2xl font-bold text-center text-white bg-purple-500 rounded hover:bg-purple-600">
                 Comparative Feedback
             </Link>
-            <Link to="/applicant" className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-purple-500 rounded shadow ripple hover:shadow-lg hover:bg-purple-600 focus:outline-none">
+            <Link to="/applicant" className="w-full max-w-xs px-4 py-2 mb-4 text-2xl font-bold text-center text-white bg-purple-500 rounded hover:bg-purple-600">
                 Applicant Crypto
             </Link>
-            <Link to="/employer" className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-purple-500 rounded shadow ripple hover:shadow-lg hover:bg-purple-600 focus:outline-none">
+            <Link to="/employer" className="w-full max-w-xs px-4 py-2 text-2xl font-bold text-center text-white bg-purple-500 rounded hover:bg-purple-600">
                 Employer Crypto
             </Link>
         </div>
