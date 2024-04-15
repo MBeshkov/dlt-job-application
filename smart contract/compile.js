e5fbf4ea10272cde265ad5ce4843b027d2434a40
MBeshkov <u05mb19@abdn.ac.uk>
2024-04-16 00:11:39 +0100

    clean up

--- smart contract/compile.js
+++ smart contract/compile.js
@@ -2,11 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-// const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
-// const source = fs.readFileSync(lotteryPath, "utf8");
-
-// module.exports = solc.compile(source, 1).contracts[":Lottery"];
-
 const activityPath = path.resolve(__dirname, "contracts", "JobActivity.sol");
 const source = fs.readFileSync(activityPath, "utf8");
 
